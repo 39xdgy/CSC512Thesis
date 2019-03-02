32334596d332239dda61b304ffa5c1b1f96088fe
Angelica <angelicafabila@protonmail.com>
2019-03-02 14:02:00 -0600

    Delete Emojis

--- ScraperPraw.py
+++ ScraperPraw.py
@@ -1,12 +1,18 @@
 import praw
+import re
 
 reddit = praw.Reddit(client_id = 'nvLZT3XnES_fbQ',
 client_secret = 'mvC2mW9qI_YDHsAW_vWXai8gMP0',  username = 'afcornell2021',
     password = 'Fhegix!00', user_agent = 'praw512')
 
-subreddit = reddit.subreddit('popular')
+subreddit = reddit.subreddit('DeepFriedMemes')
 
 hot_python = subreddit.hot(limit = 5)
+
+#def delete_emoji(String_input):
+
+# from: https://www.reddit.com/r/learnpython/comments/8br5sz/removing_emojis_from_words_python3/
+replace_emoji = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
 
 for submission in hot_python:
     if not submission.stickied:
@@ -17,6 +23,7 @@ for submission in hot_python:
 
         for comment in submission.comments.list(): #PRAW functionality
             print(20*'-')
-            print('Parent ID:', comment.parent())
-            print('Comment ID:', comment.parent())
-            print(comment.body)
+            #print('Parent ID:', comment.parent())
+            #print('Comment ID:', comment.parent())
+            #print(comment.body)
+            print(replace_emoji.sub(r'', comment.body))
